ec3e267488b774dde615c058b523dc232cc151a9
oguzp1 <oguz_paksoy_@hotmail.com>
2019-03-09 14:32:19 +0300

    added question template

--- server.py
+++ server.py
@@ -6,6 +6,59 @@
 from socket import *
 import threading
 
+questions = [
+    {
+        'id': 1,
+        'question': 'Soru 1',
+        'options': [
+            'A1',
+            'B1',
+            'C1',
+            'D1'
+        ]
+    },
+    {
+        'id': 2,
+        'question': 'Soru 2',
+        'options': [
+            'A2',
+            'B2',
+            'C2',
+            'D2'
+        ]
+    },
+    {
+        'id': 3,
+        'question': 'Soru 3',
+        'options': [
+            'A3',
+            'B3',
+            'C3',
+            'D3'
+        ]
+    },
+    {
+        'id': 4,
+        'question': 'Soru 4',
+        'options': [
+            'A4',
+            'B4',
+            'C4',
+            'D4'
+        ]
+    },
+    {
+        'id': 5,
+        'question': 'Soru 5',
+        'options': [
+            'A5',
+            'B5',
+            'C5',
+            'D5'
+        ]
+    }
+]
+
 
 class ThreadedServer:
     def listenToClient(self, client, addr):
